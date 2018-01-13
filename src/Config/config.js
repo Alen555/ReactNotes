f9b6d845d29028d189b724573b5470558ce70ce8
Alen555 <amujkic1990@hotmail.com>
2018-01-13 11:44:54 +0100

    Delete config.js

--- src/Config/config.js
+++
@@ -1,8 +0,0 @@
- export const DB_CONFIG = {
-    apiKey: "AIzaSyCFXETEX-Dz44d5LVFpeM75nXf3fpxcPDs",
-    authDomain: "reactnotes-f0ad3.firebaseapp.com",
-    databaseURL: "https://reactnotes-f0ad3.firebaseio.com",
-    projectId: "reactnotes-f0ad3",
-    storageBucket: "reactnotes-f0ad3.appspot.com",
-    messagingSenderId: "545789087740"
-  };
